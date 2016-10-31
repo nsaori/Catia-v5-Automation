c0a00bb8bc4d1d33ca02c5aaeec7cad3b4e055aa
nsaori <hana-chonsaoring0@yahoo.co.jp>
2016-10-31 15:13:49 +0900

    constraints practice

---
+++ 2dskt_constraints/Form1.cs
@@ -0,0 +1,79 @@
+﻿//p.30
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MECMOD;
+
+namespace _2dskt_constraints
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            INFITF.Application catia; //add the reference - catia v5 infiit interface...
+
+            try
+            {
+                //열려 있는 catia가져오기
+                catia = (INFITF.Application)Marshal.GetActiveObject("CATIA.Application");
+            }
+            catch (Exception)
+            {
+                //catia 실행하기
+                catia = (INFITF.Application)Activator.CreateInstance(Type.GetTypeFromProgID("CATIA.Application"));
+                catia.Visible = true;
+            }
+           
+            MECMOD.PartDocument prtDoc = (MECMOD.PartDocument)catia.Documents.Add("Part");
+            MECMOD.Part prt = prtDoc.Part;
+            MECMOD.Bodies bdys = prt.Bodies;
+            MECMOD.Body bdy = bdys.Add();
+            MECMOD.Sketches skts = bdy.Sketches;
+
+            INFITF.Reference xypln = (INFITF.Reference)prt.OriginElements.PlaneXY;
+            MECMOD.Sketch skt = skts.Add(xypln);
+
+            MECMOD.Factory2D fac2d = skt.OpenEdition();
+
+            Point2D p1 = fac2d.CreatePoint(10,10);
+            Point2D p2 = fac2d.CreatePoint(10, 40);
+            Point2D p3 = fac2d.CreatePoint(40, 40);
+            Point2D p4 = fac2d.CreatePoint(40, 10);
+            Point2D p5 = fac2d.CreatePoint(30, 20);
+            Point2D p6 = fac2d.CreatePoint(20, 5);
+
+            Line2D linLft = fac2d.CreateLine(10,10,10,40);
+            Line2D linTop = fac2d.CreateLine(10, 40, 40, 40);
+            Line2D linRgt = fac2d.CreateLine(40, 40, 40, 10);
+
+            Point2D[] parry = { p1, p6, p5, p4 };
+            Spline2D spl = fac2d.CreateSpline(parry);
+
+            linLft.StartPoint = p1;
+            linLft.EndPoint = p2;
+            linRgt.StartPoint = p3;
+            linRgt.EndPoint = p4;
+            linTop.StartPoint = p2;
+            linTop.EndPoint = p3;
+
+            //------------------
+            skt.Constraints.AddBiEltCst();
+
+            skt.CloseEdition();
+        }
+    }
+}
---
+++ sktconsPractice/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using MECMOD;
+
+namespace sktconsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            INFITF.Application catia; //add the reference - catia v5 infiit interface...
+
+            try
+            {
+                //열려 있는 catia가져오기
+                catia = (INFITF.Application)Marshal.GetActiveObject("CATIA.Application");
+            }
+            catch (Exception)
+            {
+                //catia 실행하기
+                catia = (INFITF.Application)Activator.CreateInstance(Type.GetTypeFromProgID("CATIA.Application"));
+                catia.Visible = true;
+            }
+
+            MECMOD.PartDocument prtDoc = (MECMOD.PartDocument)catia.Documents.Add("Part");
+            MECMOD.Part prt = prtDoc.Part;
+            MECMOD.Bodies bdys = prt.Bodies;
+            MECMOD.Body bdy = bdys.Add();
+            MECMOD.Sketches skts = bdy.Sketches;
+
+            INFITF.Reference xypln = (INFITF.Reference)prt.OriginElements.PlaneXY;
+            MECMOD.Sketch skt = skts.Add(xypln);
+
+            MECMOD.Factory2D fac2d = skt.OpenEdition();
+
+            Point2D p1 = fac2d.CreatePoint(50,50);
+            Point2D p2 = fac2d.CreatePoint(50, 100);
+            Point2D p3 = fac2d.CreatePoint(100, 100);
+            Point2D p4 = fac2d.CreatePoint(100, 50);
+
+            Line2D lin1 = fac2d.CreateLine(50,50,50,100);
+            Line2D lin2 = fac2d.CreateLine(50, 100, 100, 100);
+            Line2D lin3 = fac2d.CreateLine(100, 100, 100, 50);
+            Line2D lin4 = fac2d.CreateLine(100, 50, 50, 50);
+
+            lin1.StartPoint = p1;
+            lin1.EndPoint = p2;
+            lin2.StartPoint = p2;
+            lin2.EndPoint = p3;
+            lin3.StartPoint = p3;
+            lin3.EndPoint = p4;
+            lin4.StartPoint = p4;
+            lin4.EndPoint = p1;
+
+            INFITF.Reference rline1 = prt.CreateReferenceFromGeometry(lin1);
+            INFITF.Reference rline2 = prt.CreateReferenceFromGeometry(lin2);
+            INFITF.Reference rline3 = prt.CreateReferenceFromGeometry(lin3);
+            INFITF.Reference rline4 = prt.CreateReferenceFromGeometry(lin4);
+
+            INFITF.Reference rline5 = prt.CreateReferenceFromGeometry(skt.AbsoluteAxis.HorizontalReference);
+            INFITF.Reference rline6 = prt.CreateReferenceFromGeometry(skt.AbsoluteAxis.VerticalReference);
+
+            Constraint d1 = skt.Constraints.AddBiEltCst(CatConstraintType.catCstTypeDistance,rline1,rline3);
+            Constraint d2 = skt.Constraints.AddBiEltCst(CatConstraintType.catCstTypeDistance, rline2, rline4);
+            Constraint d3 = skt.Constraints.AddBiEltCst(CatConstraintType.catCstTypeDistance, rline5, rline4);
+            Constraint d4 = skt.Constraints.AddBiEltCst(CatConstraintType.catCstTypeDistance, rline6, rline1);
+
+            skt.CloseEdition();
+
+        }
+    }
+}
