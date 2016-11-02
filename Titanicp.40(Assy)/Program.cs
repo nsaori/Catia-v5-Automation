6e81c67433d323d2bee210eb589a9e6a4a9c660c
nsaori <hana-chonsaoring0@yahoo.co.jp>
2016-11-02 19:26:44 +0900

    titanic p.40 assemble copy

--- Titanicp.40(Assy)/Program.cs
+++ Titanicp.40(Assy)/Program.cs
@@ -39,14 +39,28 @@ namespace Titanicp._40_Assy_
             pdcts.AddComponentsFromFiles(tpath,"*");
 
             Product pdct1 = pdcts.AddNewProduct("ass1");
-            Products ass1 = pdct1.Products;
+            //Products ass1 = pdct1.Products;
             object[] tpath1 = 
             {
                 @"C:\Users\517-11\Desktop\saori\Automation\Catia-v5-Automation\Titanicp.40(Assy)\Assembly\Castle.CATPart",
                 @"C:\Users\517-11\Desktop\saori\Automation\Catia-v5-Automation\Titanicp.40(Assy)\Assembly\Funnel.CATPart"
             };
-            ass1.AddComponentsFromFiles(tpath1,"*");
+            pdct1.Products.AddComponentsFromFiles(tpath1, "*");
+            //ass1.AddComponentsFromFiles(tpath1,"*");
 
+            //    Product pdctRef = pdcts.AddNewProduct("p");
+            // pdctRef = pdct1.ReferenceProduct;
+            Product pdctRef = pdct1.ReferenceProduct;
+            Product pdct2 = pdcts.AddComponent(pdctRef);
+            //Product pdct2 = pdcts.AddNewProduct("p2");
+            //pdct1 = pdcts.AddComponent(pdctRef);
+            //Product pdct3 = pdcts.AddNewProduct("p3");
+            object[] aDistance = {1,0,0,0,1,0,0,0,1, 60, 0, 0 };
+            pdct2.Move.Apply(aDistance);
+
+            aDistance[9] = 120;
+            Product pdct3 = pdcts.AddComponent(pdctRef);
+            pdct3.Move.Apply(aDistance);
         }
     }
 }
