9333c5407bce7c4b1bc65b7ac52c7a640e7f7d4a
nsaori <hana-chonsaoring0@yahoo.co.jp>
2016-11-05 00:39:51 +0900

    2/3 Selection Scription

---
+++ TestingSelectionUsingScripts/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace TestingSelectionUsingScripts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(302, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get 2DPoint1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(302, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Get 2DPoint2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(302, 160);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 33);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Create 2DLines";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(302, 121);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 33);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Get 2DLine";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(302, 345);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(110, 33);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Get list of points";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(302, 202);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(110, 33);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Create 3DLines";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(302, 410);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(110, 33);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Search for Points";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Gulim", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(29, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(247, 26);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Gulim", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox2.Location = new System.Drawing.Point(29, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(247, 26);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Gulim", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox5.Location = new System.Drawing.Point(29, 417);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(163, 26);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Gulim", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox3.Location = new System.Drawing.Point(29, 128);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(247, 26);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Gulim", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox4.Location = new System.Drawing.Point(29, 246);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox4.Size = new System.Drawing.Size(247, 132);
+            this.textBox4.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 496);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ TestingSelectionUsingScripts/Form1.cs
@@ -0,0 +1,164 @@
+﻿//20161104 saori
+//p.44
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MECMOD;
+using System.Runtime.InteropServices;
+
+namespace TestingSelectionUsingScripts
+{
+    
+
+    public partial class Form1 : Form
+    {
+        INFITF.Application catia;
+        object[] arrCoord1 = new object[2];
+        object[] arrCoord2 = new object[2];
+        Point2D p1;
+        Point2D p2;
+        Line2D lin;
+        Point HyShpPtCoord1;
+        Point HyShpPtCoord2;
+        INFITF.Selection sel;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //form 실행과 동시에 카티아 실행한다---------------------------------
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                catia = (INFITF.Application)Marshal.GetActiveObject("CATIA.Application");
+            }
+            catch (Exception)
+            {
+                catia = (INFITF.Application)Activator.CreateInstance(Type.GetTypeFromProgID("CATIA.Application"));
+                catia.Visible = true;
+            }
+            PartDocument prtDoc = (PartDocument)catia.Documents.Open(@"C:\Users\517-11\Desktop\saori\Automation\Catia-v5-Automation\TestingSelectionUsingScripts\Part.CATPart");
+        }
+
+        //point1 가지고 오기-------------------------------------
+        private void button1_Click(object sender, EventArgs e)
+        {
+           // object[] arrCoord = new object[2];
+
+            try
+            {
+                sel = catia.ActiveDocument.Selection;
+                p1 = (Point2D)sel.FindObject("CATIAPoint2D");
+
+                //txt에 표시하기
+                p1.GetCoordinates(arrCoord1);
+                textBox1.Text = p1.get_Name() + " ( " + arrCoord1[0] +  " , " +arrCoord1[1] + " )";
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("선택된 정보가 없습니다.");
+                return ;
+            }
+        }
+        //point2 가져오기------------------------------------
+        private void button2_Click(object sender, EventArgs e)
+        {
+           // object[] arrCoord = new object[2];
+
+            try
+            {
+                sel = catia.ActiveDocument.Selection;
+                p2 = (Point2D)sel.FindObject("CATIAPoint2D");
+
+                p2.GetCoordinates(arrCoord2);
+                textBox2.Text = p2.get_Name() + " ( " + arrCoord2[0] + " , " + arrCoord2[1] + " )";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("선택한 정보가 없습니다.");
+                return ;
+            }
+
+        }
+        //line 가져오기-------------------------------------
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sel = catia.ActiveDocument.Selection;
+                lin = (Line2D)sel.FindObject("CATIALine2D");
+                textBox3.Text = lin.get_Name();
+
+             }
+            catch (Exception)
+            {
+                MessageBox.Show("선택한 정보가 없습니다.");
+                return;
+            }
+        }
+        //create 3DLine------------------------------------------------------
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //object[] Crd1 = new object[2];
+            //object[] Crd2 = new object[2];
+            Pobject[] CrdSt = new object[2];
+            object[] CrdEn = new object[2];
+
+            try
+            {
+                Point2D strP = lin.StartPoint;
+                Point2D endP = lin.EndPoint;
+
+                strP.GetCoordinates(CrdSt);
+                endP.GetCoordinates(CrdEn);
+
+                PartDocument prtDoc = (PartDocument)catia.ActiveDocument;
+                
+
+                GeometricElement geo2D = (GeometricElement)p1.Parent;
+                Sketch skt = (Sketch)geo2D.Parent;
+                Factory2D fac = skt.OpenEdition();
+
+                Line2D lin2 = fac.CreateLine((double)CrdSt[0], (double)CrdSt[1], (double)arrCoord1[0], (double)arrCoord1[1]);
+                lin2.StartPoint = strP;
+                lin2.EndPoint = p1;
+                Line2D lin3 = fac.CreateLine((double)CrdEn[0], (double)CrdEn[1], (double)arrCoord1[0], (double)arrCoord1[1]);
+                lin3.StartPoint = endP;
+                lin3.EndPoint = p1;
+                Line2D lin4 = fac.CreateLine((double)arrCoord1[0], (double)arrCoord1[1], (double)arrCoord2[0], (double)arrCoord2[1]);
+                lin4.StartPoint = p1;
+                lin4.EndPoint = p2;
+                //Line2D lin2 = CreateLine(fac,strP,p1) ;
+
+                skt.CloseEdition();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("선택한 정보가 없습니다.");
+                return;
+            }
+        }
+        // Create3DLine-------------------------------------------
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+        /*  // CreateLine method--------------------------------------
+ private Line2D CreateLine(Factory2D fac, Point2D pt1, Point2D pt2)
+ {
+     pt1.GetCoordinates();
+     Line2D lin = fac.CreateLine();
+ }*/
+    }
+}
