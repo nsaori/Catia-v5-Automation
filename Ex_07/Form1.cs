f39d0614baed205b5246d99d20c16f1f0b052d5f
nsaori <hana-chonsaoring0@yahoo.co.jp>
2016-11-11 23:41:59 +0900

    multi select--------------?? copy one past multi...

--- Ex_07/Form1.cs
+++ Ex_07/Form1.cs
@@ -15,6 +15,9 @@ namespace Ex_07
     public partial class Form1 : Form
     {
         INFITF.Application Catia = null;
+        INFITF.Document doc = null;
+        INFITF.Selection Sel = null;
+
 
         public Form1()
         {
@@ -32,6 +35,15 @@ namespace Ex_07
                 Catia = (INFITF.Application)Activator.CreateInstance(Type.GetTypeFromProgID("CATIA.Application"));
                 Catia.Visible = true;
             }
+
+            try
+            {
+                doc = Catia.ActiveDocument;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("활성 문서가 없습니다");
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -44,24 +56,35 @@ namespace Ex_07
                 return;
             }
 
+            /*
             // 활성 문서가 있는가?
             if (Catia.ActiveDocument == null)
             {
                 //MessageBox.Show("활성 문서가 없습니다");
                 label1.Text = "활성 문서가 없습니다";
             }
+            */
+            try
+            {
+                doc = Catia.ActiveDocument;
+            }
+            catch (Exception)
+            {
+                doc = Catia.Documents.Add("Part");
+            }
 
             // Point 선택 기능 실행
-            /*
-            INFITF.Selection Sel = null;
+           // /*
+            //INFITF.Selection Sel = null;
             Sel = Catia.ActiveDocument.Selection;
-
+            /*
             Object[] InputObjectType = { "Plane", "Line" , "Point"};
             string Status;
             Sel.Clear();
             Status = Sel.SelectElement2(InputObjectType, "Select a Elements", false);
-            Catia.ActiveDocument.Selection.Copy();
-
+            
+            //Catia.ActiveDocument.Selection.Copy();
+            Sel.Copy();
             if (Status != "Normal")
             {
                 label1.Text = "취소됨";
@@ -77,17 +100,38 @@ namespace Ex_07
             //INFITF.SelectionSets sels = (INFITF.SelectionSets)Catia.ActiveDocument.Selection;
             //ArrayList selsAlly = new ArrayList();
 
-            INFITF.Selections sels = ;
-            object[] arr = new object[100];
-            sels.GetListOfSelectionSet(arr);
-            Catia.ActiveDocument.Selection.Copy();
+            //INFITF.Selections sels = ;
+            //object[] arr = new object[100];
+            //sels.GetListOfSelectionSet(arr);
+            //Catia.ActiveDocument.Selection.Copy();
 
+            /*
+            INFITF.Selection sel = Catia.ActiveDocument.Selection;
+            INFITF.Selection sels = null;
+            INFITF.AnyObject[] selObj;
+            INFITF.SelectedElement sele = null;
+            ///*
+            int i = 0;
+            while ((sel = Catia.ActiveDocument.Selection) != null)
+            {
+                i++;
+                // selObj.a
+                sels.Item(i);
+            }
+            for (int i = 1;i<=sel.Count2 ; i++)
+            {
+                
+            }
 
+            */
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Catia.ActiveDocument.Selection.Paste();
+            //Catia.ActiveDocument.Selection.Paste();
+            Sel.Paste();
+
+            
         }
     }
 }
